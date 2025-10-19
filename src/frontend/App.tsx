46db2fc264d0a119f7bcb7cc58241567fb645db7
pal4569 <michaelfc8841@gmail.com>
2025-10-19 13:31:57 -0400

    ready for backend deployment

---
+++ src/frontend/App.tsx
@@ -0,0 +1,48 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import SideBar from "./components/SideBar/SideBar";
+import Landing from "./pages/Landing/Landing";
+import BlogEditor from "./pages/BlogEditor/BlogEditor";
+import LoadBlog from "./pages/LoadBlog/LoadBlog";
+import { Outlet } from "react-router-dom";
+import { SidebarProvider } from "./components/SideBar/SideBarContent/SidebarProvider";
+import VideoPlayer from "./pages/VideoPlayer/VideoPlayer";
+import LoginPage from "./pages/LoginPage/LoginPage";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <SidebarProvider>
+          <div className="layout">
+            <SideBar />
+            <main className="main-content">
+              <Outlet />
+            </main>
+          </div>
+        </SidebarProvider>
+      ),
+      children: [
+        { index: true, element: <Landing /> },
+        { path: "blogs/new", element: <BlogEditor /> },
+        { path: "blogs/edit/:id", element: <BlogEditor /> },
+        { path: "create/:id", element: <BlogEditor /> },
+        { path: "blogs/:id", element: <LoadBlog /> },
+        { path: "videos/:id", element: <VideoPlayer /> },
+        { path: "login", element: <LoginPage /> },
+      ],
+    },
+  ],
+  {
+    basename: import.meta.env.MODE === "production" ? "/personal-site" : "/",
+  }
+);
+
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
